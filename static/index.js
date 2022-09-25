8657c0fb4dc8bf8bbc24cfe136f30f10805a6a3f
Cicada Carpenter <cicada.carpenter@gmail.com>
2022-09-25 13:11:17 -0600

    Update Javascript

--- static/index.js
+++ static/index.js
@@ -1,4 +1,11 @@
 function validation(){
-    var username;
-    var username_error;
+    var username = document.getElementById("username");
+    var username_error = document.getElementById("error_username");
+
+    if (username.value.length <= 5) {
+         username_error.innerHTML = "Username is too short.";
+         username_error.style.color = "#ff0000";
+
+         return false;
+    }
 }
