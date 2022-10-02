2f222084282de101726196c169ed80972edda7fa
Cicada Carpenter <cicada.carpenter@gmail.com>
2022-10-02 13:12:02 -0600

    Server-side validation

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import flask
+from flask import request
 app = flask.Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['post', 'get'])
 def login():
-    return flask.render_template('index.html')
+    message = ''
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == 'testusername' and password == 'testpassword':
+            message = "Correct username and password"
+            return flask.render_template('welcome.html', message=message)
+        else:
+            message = "Wrong username or password"
+
+    return flask.render_template('index.html', message=message)
+    
--- static/index.js
+++ static/index.js
@@ -2,10 +2,20 @@ function validation(){
     var username = document.getElementById("username");
     var username_error = document.getElementById("error_username");
 
-    if (username.value.length <= 5) {
+    
+    if (username.value == "") {
+        username_error.innerHTML = "Username field is empty.";
+        username_error.style.color = "#ff0000";
+
+        return false;
+    }
+    else if (username.value.length <= 5) {
          username_error.innerHTML = "Username is too short.";
          username_error.style.color = "#ff0000";
 
          return false;
+    }
+    else {
+        username_error.textContent = "";
     }
 }
